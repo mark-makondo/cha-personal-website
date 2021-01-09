87e4ff34994de55d1ed4d8df754b51ba31abc629
Mark Makondo <markalbert.makondo@gmail.com>
2021-01-09 21:36:07 +0800

    included sass

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+// svg paths
+import {ReactComponent as ChaBeautyLogo} from '../img/cha-beauty-logo.svg';
+
 const header = () => {
 
     return(
         <div className="header">
-            this is a header
+           <ChaBeautyLogo fill="#707070"/> 
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et dolor enim necessitatibus recusandae. Voluptates beatae, facilis dignissimos nam amet nostrum, dolorem quas reiciendis iure magnam, accusamus debitis voluptatum quam et.
+            </p>
+     
         </div>
     )
 }
-
 export default header;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+// svg paths
+
+import {ReactComponent as NavLines} from '../img/nav-lines.svg';
+import {ReactComponent as NavCircle} from '../img/nav-circle.svg';
+
 const navbar = () => {
+
+    const nav__contents = () => {
+        return(
+            <ul>
+                <li>Home</li>
+                <li>Features</li>
+                <li>Contact Me</li>
+            </ul>
+        )
+    }
 
     return(
         <div className="navbar">
-            this is the navbar
+            <NavLines />
+            <NavCircle />
         </div>
     )
 }
