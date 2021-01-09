57195e7a5728671f5a76fc952ff2bd9ccf17ab88
Mark Makondo <markalbert.makondo@gmail.com>
2021-01-09 23:46:41 +0800

    content plotted done

--- src/components/featured.js
+++ src/components/featured.js
@@ -4,7 +4,39 @@ const featured = () => {
 
     return(
         <div className="featured">
-            this is the featured
+            <span className="featured__main-title">Featured Works</span>
+
+            <div className="featured__container">
+                <div className="featured__hair">
+                    <div className="featured__box">
+                    </div>
+                    <span className="featured__sub-title">Hair</span>
+                </div>
+
+                <div className="featured__beauty-permanent">
+                    <div className="featured__box">
+                    </div>
+                    <span className="featured__sub-title">Beauty Permanent</span>
+                </div>
+
+                <div className="featured__eyebrows">
+                    <div className="featured__box">
+                    </div>
+                    <span className="featured__sub-title">Eyebrows</span>
+                </div>
+
+                <div className="featured__nailcare">
+                    <div className="featured__box">
+                    </div>
+                    <span className="featured__sub-title">Nail Care</span>
+                </div>
+
+                <div className="featured__waxing">
+                    <div className="featured__box">
+                    </div>
+                    <span className="featured__sub-title">Waxing</span>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+// svg paths
+import {ReactComponent as ChaBeautyLogo} from '../img/cha-beauty-logo.svg';
+
 const footer = () => {
 
     return(
         <div className="footer">
-            this is a footer
+            <div className="footer__container">
+                <span className="footer__title">Do you want to see more?</span>
+                <div className="footer__logo">
+                    <ChaBeautyLogo fill="#707070" width="100%"/> 
+                </div> 
+                <div className="footer__contact">
+
+                </div>
+                <span className="footer__copyright">Copyright &#x24B8; Chanda Makondo</span>
+            </div>
         </div>
     )
 }
